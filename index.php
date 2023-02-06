7c16b5e2a72c35fda827139e158f1791f71b8c5f
i-ahumada <ahumadaivan.cpp@gmail.com>
2023-02-06 15:32:08 -0300

    Responsive

--- index.php
+++ index.php
@@ -8,90 +8,93 @@
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <div class="calc-container py-2 px-3">
-            <form action="calculate.php" method="post">
-                <div class="row mb-3 mt-2">
-                    <div class="col-12 text-right">
-                        <textarea class="h-100 w-100" type="text" name="display" id="display" placeholder="->"
-                        onkeydown="return false"><?php 
-                                if(isset($_GET['resultado'])) {
-                                    echo $_GET['resultado'];
-                                }
-                                ?></textarea>
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="calc-container py-2 px-3">
+                <form action="calculate.php" method="post">
+                    <div class="row mb-3 mt-2">
+                        <div class="col-12 text-right">
+                            <textarea class="h-100 w-100" type="text" name="display" id="display" placeholder="->"
+                            onkeydown="return false"><?php 
+                                    if(isset($_GET['resultado'])) {
+                                        echo $_GET['resultado'];
+                                    }
+                                    ?></textarea>
+                        </div>
                     </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="" disabled>
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="" disabled>
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="[]^[]" disabled>
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100" type="button" value="CE" id="clear-everything">
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100" type="button" value="C" id="clear">
+                        </div>
                     </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="[]^[]" disabled>
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="1" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="2" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="3" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="+" >
+                        </div>
                     </div>
-                    <div class="col-3">
-                        <input class="w-100" type="button" value="CE" id="clear-everything">
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="4" > 
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="5" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="6" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="-" >
+                        </div>
                     </div>
-                    <div class="col-3">
-                        <input class="w-100" type="button" value="C" id="clear">
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="7" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="8" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="9" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="/" >
+                        </div>
                     </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="1" >
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <input class="w-100 calc-button"  type="button" value="00" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="0" >
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100" type="submit" value="=" id="equal-submit">
+                        </div>
+                        <div class="col-3">
+                            <input class="w-100 calc-button" type="button" value="*" >
+                        </div>
                     </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="2" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="3" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="+" >
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="4" > 
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="5" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="6" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="-" >
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="7" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="8" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="9" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="/" >
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-3">
-                        <input class="w-100 calc-button"  type="button" value="00" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="0" >
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100" type="submit" value="=" id="equal-submit">
-                    </div>
-                    <div class="col-3">
-                        <input class="w-100 calc-button" type="button" value="*" >
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
+
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <script src="js/main.js"></script>
